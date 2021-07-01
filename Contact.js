f56910b2a996ea1bd1cb00d2690b1cdd0f660a33
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-01 23:29:38 +0530

    Ability To Ensure Valid Contacts Are Added

--- Contact.js
+++ Contact.js
@@ -7,49 +7,89 @@ class Contact {
     zip;
     phoneNumber;
     email;
-  
+
     constructor(...params) {
-      this.firstName = params[0];
-      this.lastName = params[1];
-      this.address = params[2];
-      this.city = params[3];
-      this.state = params[4];
-      this.zip = params[5];
-      this.phoneNumber = params[6];
-      this.email = params[7];
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
     }
-  
-    get firstName() {return this._firstName;}
-    set firstName(firstName) {this._firstName =firstName;}
-    
-    get lastName() {return this._lastName;}
-    set lastName(lastName) {this._lastName = lastName;}
-  
-    get address() {return this._address;}
-    set address(address) {this._address = address;}
-  
-    get city() {return this._city;}
-    set city(city) {this._city = city;}
-  
-    get state() {return this._state;}
-    set state(state) {this._state = state;}
-  
-    get zip() {return this._zip;}
-    set zip(zip) {this._zip = zip;}
-  
-    get phoneNumber() {return this._phoneNumber;}
-    set phoneNumber(phoneNumber) {this._phoneNumber = this.phoneNumber;}
-  
-    get email() {return this._email;}
-    set email(email) {this._email = email;}
-  
-  
-  
-      toString() {
-         return "FirstName = "+ this.firstName +", LastName = "+this.lastName+", Address = "+this.address+", City ="+this.city+
-         ", State = "+this.state+", Zip= "+this.zip+", PhoneNumber="+this.phoneNumber+", Email="+this.email;
-      }
-  }
-  
-  let contact=new Contact("Amit","Shah","patna",'PATNA','BIHAR','852014','78541232','amit@gmail.com');
-  console.log(contact)
+
+
+    get firstName() { return this._firstName; }
+    set firstName(firstName) {
+        let firstNameRegex = RegExp('^[A-Z]{1}[a-z]{1,}$');
+        if (firstNameRegex.test(firstName))
+            this._firstName = firstName;
+        else throw 'Invalid FirstName';
+    }
+
+    get lastName() { return this._lastName; }
+    set lastName(lastName) {
+        let lastNameRegex = RegExp('^[A-Z]{1}[a-z]{1,}$');
+        if (lastNameRegex.test(lastName))
+            this._lastName = lastName;
+        else throw 'Invalid LastName '
+    }
+
+
+    get address() { return this._address; }
+    set address(address) {
+        let addressRegex = RegExp('^[0-9 A-Z]{1}[A-Z a-z 0-9]{1,}$');
+        if (addressRegex.test(address))
+            this._address = address;
+        else throw 'Invalid Address'
+    }
+
+    get city() { return this._city; }
+    set city(city) {
+        let cityRegex = RegExp('^[A-Z]{1}[A-Z a-z]{1,}$');
+        if (cityRegex.test(city))
+            this._city = city;
+        else throw 'Invalid City Name'
+    }
+
+    get state() { return this._state; }
+    set state(state) {
+        let stateRegex = RegExp('^[A-Z]{1}[A-Z a-z]{1,}$');
+        if (stateRegex.test(state))
+            this._state = state;
+        else throw 'Invalid State Name'
+    }
+
+    get zip() { return this._zip; }
+    set zip(zip) {
+        let zipRegex = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else throw 'Invalid Zip'
+    }
+
+    get phoneNumber() { return this._phoneNumber; }
+    set phoneNumber(phoneNumber) {
+        let phoneNumberRegex = RegExp('^[1-9]{2}\\s{0,1}[1-9]{1}[0-9]{9}$');
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNumber = this.phoneNumber;
+        else throw 'Invalid PhoneNumber'
+    }
+
+    get email() { return this._email; }
+    set email(email) {
+        let emailRegex = RegExp('^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}([.][a-z A-Z]{2})$');
+        if (emailRegex.test(email))
+            this._email = email;
+        else throw 'Invalid Email'
+    }
+
+    toString() {
+        return "FirstName = " + this.firstName + ", LastName = " + this.lastName + ", Address = " + this.address + ", City =" + this.city +
+            ", State = " + this.state + ", Zip= " + this.zip + ", PhoneNumber=" + this.phoneNumber + ", Email=" + this.email;
+    }
+}
+
+let contact = new Contact("Amit", "Shah", "patna", 'PATNA', 'BIHAR', '852014', '78541232', 'amit@gmail.com');
+console.log(contact)
